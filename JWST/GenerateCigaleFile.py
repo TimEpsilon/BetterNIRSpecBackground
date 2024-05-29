4d050d61b16016888e106772e37b77d54ada290e
TimEpsilon <tim.dewachter@live.fr>
2024-05-29 12:03:37 +0200

    Added code to generate a Cigale input file on demand

---
+++ JWST/GenerateCigaleFile.py
@@ -0,0 +1,43 @@
+import os
+from glob import glob
+from astropy.io import fits
+from astropy.table import QTable
+from utils import logConsole
+
+
+working_dir = "./mastDownload/JWST/"
+
+def iterateOverFolders():
+	for folder in glob(working_dir + "*"):
+		folder = f"{folder}/Final/"
+		print(f"Working on {folder}")
+		generateCigaleFile(folder)
+
+
+def generateCigaleFile(folder):
+	ids = []
+	paths = []
+	modes = []
+
+	for file in glob(os.path.join(folder, '*_x1d.fits')):
+		data = fits.open(file)
+
+		# Ignoring none 3 shutter slits because they have not been treated
+		if len(data[1].header["SHUTSTA"]) != 3:
+			continue
+
+		ids.append(f"nirspec_{data[1].header['SOURCEID']}")
+		paths.append(file)
+		modes.append(data[0].header["GRATING"].lower())
+
+	redshift = [-1 for _ in range(len(ids))]
+	norm = ["wave" for _ in range(len(ids))]
+
+	table = QTable([ids, redshift, paths, modes, norm],
+				   names=('id', 'redshift', 'spectrum', 'mode', 'norm'))
+
+	print("Saving Table")
+	table.write(folder + 'cigale-data.fits', overwrite=True)
+
+
+iterateOverFolders()
