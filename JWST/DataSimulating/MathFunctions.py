976bc9413be8b8333ceccafdd908441fa1df05d0
TimEpsilon <tim.dewachter@live.fr>
2024-10-08 17:25:15 +0200

    Did some math tests

--- JWST/DataSimulating/MathFunctions.py
+++ JWST/DataSimulating/MathFunctions.py
@@ -1,18 +1,66 @@
 import numpy as np
 from scipy.interpolate import interp1d
 
+
+def add2Dto1D(array2D, array1D):
+	"""
+	Creates a 3D array, where each element of the 1D array is added to a copy of the 2D array
+	Parameters
+	----------
+	array1D
+	array2D
+
+	Returns :
+	array3D
+	-------
+	"""
+	if isinstance(array1D,int):
+		array1D = np.array([array1D])
+	elif isinstance(array1D,list):
+		array1D = np.array(array1D)
+
+	if isinstance(array2D,float) or isinstance(array2D,int):
+		array2D = np.array([[array2D]])
+	return array2D[:, :, np.newaxis] + array1D[np.newaxis, np.newaxis, :]
+
 def gauss(x, x0, sigma):
 	return np.exp(-(x-x0)**2/(2*sigma**2))
 
 def lorentzian(x, x0, L):
-	return L/(2*np.pi) / (L**2/4 + (x-x0)**2)
+	# x0 can be a single point or a list
+	# should be of size Dx,Dy,len(x0)
+	X = add2Dto1D(x,-x0)
+	return (L/(2*np.pi) / (L**2/4 + (X)**2)).sum(axis=2)
 
 def continuum(x,x0, y0):
 	# x0 and y0 are a list of numbers
 	interp = interp1d(x0,y0,kind='cubic')
 	return interp(x)
 
-def signal(x,y,peaks=[],sigma=10,Lwidth=1):
-	# TODO
-	result = 0
+def signal(x, y, continuumX=None, continuumZ=None, peaks=None, Lwidth=1, sigma=10):
+	"""
+	Generates the image of a signal with lorentzian peaks, a smooth continuum and a gaussian spatial envelope
+	Parameters
+	----------
+	x : abscissa, wavelength dependant
+	y : ordinate, spatial dependant
+	continuumX :
+	continuumZ
+	sigma
+	Lwidth
+
+	Returns
+	-------
+
+	"""
+	# Default : 3 points
+	if continuumZ is None:
+		continuumZ = [0, 1, 0]
+	if continuumX is None:
+		continuumX = [x[0], (x[-1] + x[0]) / 2, x[-1]]
+	if peaks is None:
+		peaks = [(x[-1] + x[0]) / 2]
+
+	results = (continuum(x, continuumX, continuumZ) + lorentzian(peaks,0.2)) * gauss(y,(y.max() + y.min())/2, 2)
+	return results
 
---
+++ JWST/DataSimulating/Test/MathFunctions_UT.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append("../MathFunctions")
+
+import unittest
+import numpy as np
+from DataSimulating import MathFunctions
+
+class DataTest_UT(unittest.TestCase):
+	def testAdd2Dto1D(self):
+		array2D = np.array([[1,2],
+							[3,4],
+							[5,6]])
+
+		array1D = np.array([1])
+		expect = np.array([[[2],[3]],
+						   [[4],[5]],
+						   [[6],[7]]])
+		result = MathFunctions.add2Dto1D(array2D,array1D)
+		self.assertEqual( True, np.all(expect==result), msg=f"Expected {expect} but got {result}")
+
+
+		array1D = [1]
+		result = MathFunctions.add2Dto1D(array2D, array1D)
+		self.assertEqual(True, np.all(expect == result), msg=f"Expected {expect} but got {result}")
+
+
+		array1D = 1
+		result = MathFunctions.add2Dto1D(array2D, array1D)
+		self.assertEqual(True, np.all(expect == result), msg=f"Expected {expect} but got {result}")
+
+
+		array1D = np.array([1,2])
+		expect = np.array([[[2,3],[3,4]],
+						   [[4,5],[5,6]],
+						   [[6,7],[7,8]]])
+		result = MathFunctions.add2Dto1D(array2D, array1D)
+		self.assertEqual( True, np.all(expect==result), msg=f"Expected {expect} but got {result}")
+
+
+		array1D = np.array([1, 2, 3])
+		expect = np.array([[[2, 3, 4], [3, 4, 5]],
+						   [[4, 5, 6], [5, 6, 7]],
+						   [[6, 7, 8], [7, 8, 9]]])
+		result = MathFunctions.add2Dto1D(array2D, array1D)
+		self.assertEqual( True, np.all(expect==result), msg=f"Expected {expect} but got {result}")
+
+
+		print("Test for add2Dto1D passed")
+
+
+	def testGauss(self):
+		x = 3
+		y = MathFunctions.gauss(x,3,1)
+		self.assertEqual(1,y)
+
+		x = 4
+		y = MathFunctions.gauss(x,3,1)
+		self.assertEqual(np.e**(-1/2),y)
+
+		print("Test for gauss passed")
+
+
+	def testLorentzian(self):
+		x = 3
+		y = MathFunctions.lorentzian(x,3,0.5)
+		self.assertEqual(np.array([[2/(0.5*np.pi)]]),y)
+
+		x = 3.25
+		y = MathFunctions.lorentzian(x, 3, 0.5)
+		self.assertEqual(np.array([[1 / (0.5 * np.pi)]]), y)
+
+		print("Test for lorentzian passed")
+
+	def testSignal(self):
+		#TODO
+		x = np.linspace(0.6,5,500)
+		y = np.linspace(0,1,100)
+
+if __name__ == '__main__':
+	unittest.main()
