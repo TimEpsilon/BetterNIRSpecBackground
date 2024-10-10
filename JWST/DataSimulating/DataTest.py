b55e932487f69cd56d0c9ac321889149727b263b
TimEpsilon <tim.dewachter@live.fr>
2024-10-10 17:56:31 +0200

    Started backwards process

--- JWST/DataSimulating/DataTest.py
+++ JWST/DataSimulating/DataTest.py
@@ -27,7 +27,7 @@ class TestSlit:
 
 class TestSlitlet:
 	def __init__(self, Xsize, Ysize, sigmaNoise=1,
-				 continuumX=None, continuumZ=None, peaks=None,
+				 continuumX=None, continuumZ=None, signalX=None, signalZ=None, peaks=None,
 				 peaksAmp=1, Lwidth=1, sigmaEnvelope=10, rotationAngle=30):
 		"""
 		Creates a Slitlet, composed of 3 slits
@@ -50,8 +50,9 @@ class TestSlitlet:
 		dy = int(Ysize/3)
 		self.background = TestSlit(Xsize, dy, MathFunctions.background,
 								   continuumX=continuumX, continuumZ=continuumZ, sigma=sigmaEnvelope)
-		self.signal = TestSlit(Xsize, dy, MathFunctions.signal, continuumX=continuumX, continuumZ=continuumZ,
+		self.signal = TestSlit(Xsize, dy, MathFunctions.signal, continuumX=signalX, continuumZ=signalZ,
 							   sigma=sigmaEnvelope, A=peaksAmp, peaks=peaks, Lwidth=Lwidth)
+		self.signal.data += self.background.data
 
 		template = np.zeros((Ysize, Xsize))
 
@@ -74,9 +75,9 @@ class TestSlitlet:
 
 		# Rotating
 
-		self.rotated_100 = scipy.ndimage.rotate(self.slitlet_100, self.rotationAngle,cval=np.nan)
-		self.rotated_010 = scipy.ndimage.rotate(self.slitlet_010, self.rotationAngle,cval=np.nan)
-		self.rotated_001 = scipy.ndimage.rotate(self.slitlet_001, self.rotationAngle,cval=np.nan)
+		self.rotated_100 = scipy.ndimage.rotate(self.slitlet_100, self.rotationAngle,cval=np.nan,order=0)
+		self.rotated_010 = scipy.ndimage.rotate(self.slitlet_010, self.rotationAngle,cval=np.nan,order=0)
+		self.rotated_001 = scipy.ndimage.rotate(self.slitlet_001, self.rotationAngle,cval=np.nan,order=0)
 
 		# Adding noise
 
@@ -87,28 +88,89 @@ class TestSlitlet:
 		self.data_010 = self.rotated_010 + self.noise
 		self.data_001 = self.rotated_001 + self.noise
 
-	def show(self, ID):
-		if ID == "100":
-			plt.figure(figsize=(12,6))
-			plt.imshow(self.data_100, origin="lower",)
+		# Anti rotation
 
-		if ID == "010":
-			plt.figure(figsize=(12, 6))
-			plt.imshow(self.data_010, origin="lower")
+		self.antiRotated_100 = scipy.ndimage.rotate(self.data_100, -self.rotationAngle,cval=np.nan,order=0)
+		rows = np.any(~np.isnan(self.antiRotated_100), axis=1)
+		cols = np.any(~np.isnan(self.antiRotated_100), axis=0)
+		self.antiRotated_100 = self.antiRotated_100[rows][:, cols]
 
-		if ID == "001":
-			plt.figure(figsize=(12, 6))
-			plt.imshow(self.data_001, origin="lower")
+		self.antiRotated_010 = scipy.ndimage.rotate(self.data_010, -self.rotationAngle,cval=np.nan,order=0)
+		rows = np.any(~np.isnan(self.antiRotated_010), axis=1)
+		cols = np.any(~np.isnan(self.antiRotated_010), axis=0)
+		self.antiRotated_010 = self.antiRotated_010[rows][:, cols]
 
+		self.antiRotated_001 = scipy.ndimage.rotate(self.data_001, -self.rotationAngle,cval=np.nan,order=0)
+		rows = np.any(~np.isnan(self.antiRotated_001), axis=1)
+		cols = np.any(~np.isnan(self.antiRotated_001), axis=0)
+
+		self.antiRotated_001 = self.antiRotated_001[rows][:, cols]
+
+		# Anti Rotated Noise Map
+
+		self.antiNoise_100 = self.antiRotated_100 - self.slitlet_100
+		self.antiNoise_010 = self.antiRotated_010 - self.slitlet_010
+		self.antiNoise_001 = self.antiRotated_001 - self.slitlet_001
+
+	def show(self, TYPE):
+
+		if TYPE == "RAW":
+			plt.figure(figsize=(12,18))
+			plt.subplot(3,1,1)
+			plt.imshow(self.data_100, origin="lower",vmin=0)
+			plt.subplot(3, 1, 2)
+			plt.imshow(self.data_010, origin="lower",vmin=0)
+			plt.subplot(3, 1, 3)
+			plt.imshow(self.data_001, origin="lower",vmin=0)
+
+		if TYPE == "REAL":
+			plt.figure(figsize=(12,18))
+			plt.subplot(3, 1, 1)
+			plt.imshow(self.slitlet_100, origin="lower",vmin=0)
+			plt.subplot(3, 1, 2)
+			plt.imshow(self.slitlet_010, origin="lower",vmin=0)
+			plt.subplot(3, 1, 3)
+			plt.imshow(self.slitlet_001, origin="lower",vmin=0)
+
+		if TYPE == "CORRECTED":
+			plt.figure(figsize=(12,18))
+			plt.subplot(3, 1, 1)
+			plt.imshow(self.antiRotated_100, origin="lower",vmin=0)
+			plt.subplot(3, 1, 2)
+			plt.imshow(self.antiRotated_010, origin="lower",vmin=0)
+			plt.subplot(3, 1, 3)
+			plt.imshow(self.antiRotated_001, origin="lower",vmin=0)
+
+		if TYPE == "ANTINOISE":
+			plt.figure(figsize=(12, 18))
+			plt.subplot(3, 1, 1)
+			plt.imshow(self.antiNoise_100, origin="lower", vmin=0)
+			plt.subplot(3, 1, 2)
+			plt.imshow(self.antiNoise_010, origin="lower", vmin=0)
+			plt.subplot(3, 1, 3)
+			plt.imshow(self.antiNoise_001, origin="lower", vmin=0)
 		return
 
+	@staticmethod
+	def analyseNoise(image):
+		hist,bins = np.histogram(image, bins=256)
+		plt.bar(bins,hist)
+
+
 slitlet = TestSlitlet(2000, 300, 30,
-					  continuumX=[0,2000,400,1300], continuumZ=[10,200,100,160],
+					  continuumX=[0,2000,400,1300], continuumZ=[200,10,140,100],
+					  signalX=[0,2000,1400,600], signalZ=[150,300,240,160],
 					  peaks=[500,550,650,1400,1430,1900], peaksAmp=[1000, 1200, 750, 1700, 1750, 600], Lwidth=5,
-					  sigmaEnvelope=10, rotationAngle=12)
+					  sigmaEnvelope=20, rotationAngle=12)
 
 plt.close("all")
-slitlet.show("100")
-slitlet.show("010")
-slitlet.show("001")
+slitlet.show("RAW")
+slitlet.show("REAL")
+slitlet.show("CORRECTED")
+slitlet.show("ANTINOISE")
+
+plt.figure()
+TestSlitlet.analyseNoise(slitlet.antiNoise_100)
+TestSlitlet.analyseNoise(slitlet.antiNoise_010)
+TestSlitlet.analyseNoise(slitlet.antiNoise_001)
 plt.show()
